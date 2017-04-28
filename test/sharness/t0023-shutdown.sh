3d48d5a29507dd64df4a7e6b9185170b7cf80dae
Jeromy <jeromyj@gmail.com>
2017-04-27 19:04:23 -0700

    Implement ipfs shutdown command
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

---
+++ test/sharness/t0023-shutdown.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Copyright (c) 2017 Jeromy Johnson
+# MIT Licensed; see the LICENSE file in this repository.
+#
+
+test_description="Test shutdown command"
+
+. lib/test-lib.sh
+
+test_init_ipfs
+
+test_launch_ipfs_daemon
+
+test_expect_success "shutdown succeeds" '
+	ipfs shutdown
+'
+
+test_expect_success "daemon no longer running" '
+	test_expect_code  1 kill -0 $IPFS_PID
+'
+
+test_launch_ipfs_daemon --offline
+
+test_expect_success "shutdown succeeds" '
+	ipfs shutdown
+'
+
+test_expect_success "daemon no longer running" '
+	test_expect_code  1 kill -0 $IPFS_PID
+'
+test_done
